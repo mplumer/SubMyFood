1611f93942f03194936ba260143da4cfa7a013ed
Max Plumer <68719068+mplumer@users.noreply.github.com>
2020-09-21 16:30:25 -0400

    update, add readme and test.js

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # SubMyFood
+
+## User Story:
+AS A student preparing my meal
+I WANT to be able to substitute an ingredient I do not have 
+SO THAT I can still cook a meal without the missing ingredient
+
+## Acceptance Criteria:
+GIVEN A search input 
+WHEN I search for an ingredient and choose from the dropdown how many recipe options to display
+THEN I am presented with a list of recipes 
+WHEN I want to substitue an ingredient on the recipe list
+THEN I can click the button on the ingredients card to search the ingredient I  want to substitute and get an alternative ingredient
+WHEN I refresh the page
+THEN My previous search options will be saved in the Saved Recipes drop-down
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+* Bulma
+* Recipe Puppy API
+* Meal-Hero API
+
+## GitHub Repository:
+https://github.com/random-group-name/SubMyFood.git
+
+## GitHub Deployed Link:
+https://random-group-name.github.io/SubMyFood/ 
+
+## Screenshots and updates of project:
+
+![Screenshot](assets/images/Screenshot1.png)
+![Screenshot](assets/images/screenshot2.png)
+![Screenshot](assets/images/screenshot3.png)
+![Screenshot](assets/images/screenshot4.png)
+
+## Contribution
+* Alexandra Nance
+* Mera Youssef
+* Maxwell Plumer
---
+++ assets/js/test.js
@@ -0,0 +1,74 @@
+
+document.getElementById("submitBtn").addEventListener("click", function () {
+
+    // Get the recipe the user typed in
+    var recipe = document.querySelector("#recipeName").value;
+    console.log(recipe);
+    // Save the recipe in local storage
+
+    // Save the recipe under Saved Recipes tab
+
+    // Search for the recipe using the recipe-puppy api
+    fetch("https://recipe-puppy.p.rapidapi.com/?q=" + recipe, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
+            "x-rapidapi-key": "9ce51bbfeamsh6ee4d89a931f3b7p116f4bjsn94071dc14327"
+        }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(response => {
+    
+        // Get how many recipes the user would like to see
+        var numChoice = document.getElementById("mySelect").value;
+    
+        for(var i=0; i<= (numChoice-1); i++) {
+            console.log(response.results[i].ingredients);
+
+            // create header for name of recipe
+            // create list for ingredients
+            // maybe a pic
+
+        }
+        git
+
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+    fetch("https://meal-hero.p.rapidapi.com/api/annotations/foods/"+ recipe + "/substitutions", {
+        "method": "GET",
+        "headers": {
+                "x-rapidapi-host": "meal-hero.p.rapidapi.com",
+                "x-rapidapi-key": "9ce51bbfeamsh6ee4d89a931f3b7p116f4bjsn94071dc14327"
+        }
+        })
+        .then(response => {
+            
+            return response.json();
+        })
+        .then(response => {
+            
+            for(var i = 0; i <response.food_substitutions.length; i++) {
+                console.log(response.food_substitutions[i].substitute.entity);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+});
+
+document.getElementById("cancelBtn").addEventListener("click", function () {
+    console.log("I was clicked!");
+});
+
+/* Features to discuss
+- do we need the cancel button
+- do we want the recipes to append to the page or go to another page?
+- do we want the ingredients in the recipes to be clickable or do we want a 
+separate search field for substitutions?
+*/
